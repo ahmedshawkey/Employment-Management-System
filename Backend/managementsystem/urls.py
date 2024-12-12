a15146b9f41a35180d5a44e08b4221906d66ccad
Ahmed Shawkey <ahmed99shawkey@gmaill.com>
2024-12-12 17:48:14 +0200

    Configuring the frontend react app to build its static files in the templates folder in the django app

--- Backend/managementsystem/urls.py
+++ Backend/managementsystem/urls.py
@@ -17,7 +17,11 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 
+from django.views.generic import TemplateView
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include('APIs.urls')),
+    path('', TemplateView.as_view(template_name="index.html")),
+    path('<path:path>', TemplateView.as_view(template_name="index.html")),
 ]
